2e3b7e440b1376f8e8f8492b5b660016b8b56b34
Marius Ungureanu <marius.ungureanu@xamarin.com>
2018-02-26 13:32:43 +0200

    Refactor .cs into a constant

--- src/Gir/Generation/IGeneratableExtensions.cs
+++ src/Gir/Generation/IGeneratableExtensions.cs
@@ -5,9 +5,11 @@ namespace Gir
 {
 	public static class IGeneratableExtensions
 	{
+		const string CSharpFileExtension = ".cs";
+
 		public static IndentWriter GetWriter (this IGeneratable gen, GenerationOptions opts)
 		{
-			var path = Path.Combine(opts.DirectoryPath, gen.Name + ".cs");
+			var path = Path.Combine(opts.DirectoryPath, gen.Name + CSharpFileExtension);
 			return IndentWriter.OpenWrite(path, opts);
 		}
 	}
