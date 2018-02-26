072830c1c0591b9201133fe554c4307e46cfb5a3
Jo Shields <joshield@microsoft.com>
2018-02-26 14:09:17 -0500

    Move CI scripts to Git

---
+++ .jenkins.groovy
@@ -0,0 +1,22 @@
+def builders = [:]
+// labels to build on
+def alltargets = ["ubuntu-1604-amd64","ubuntu-1604-i386"]
+
+for (x in alltargets) {
+    def mainlabel = x
+    builders[mainlabel] = {
+        stage("Test on ${mainlabel}") {
+            ws {
+                git credentialsId: '56f5f1ca-b5a7-41b5-8318-c84dd4364e72', url: 'git://github.com/mono/gir-sharp.git'
+                chroot additionalPackages: 'ca-certificates-mono nuget msbuild mono-devel libgtk2.0-dev libgtk-3-dev', chrootName: "${mainlabel}-stable", command: "nuget restore src/Gir.sln && nuget install NUnit.ConsoleRunner -Version 3.8.0 -OutputDirectory testrunner && msbuild /p:Configuration=Release src/Gir.sln && mono ./testrunner/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe ./src/Gir.Tests/bin/Release/Gir.Tests.dll"
+                step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 1, thresholds: [[$class: 'FailedThreshold', failureNewThreshold: '5', failureThreshold: '5', unstableNewThreshold: '1', unstableThreshold: '1'], [$class: 'SkippedThreshold', failureNewThreshold: '5', failureThreshold: '5', unstableNewThreshold: '1', unstableThreshold: '1']], tools: [[$class: 'NUnit3JunitHudsonTestType', deleteOutputFiles: true, failIfNotNew: true, pattern: '**/TestResult.xml', skipNoTestFiles: true, stopProcessingIfError: true]]])
+            }
+        }
+    }
+}
+
+timestamps {
+    node(alltargets[0]) {
+        parallel builders
+    }
+}
