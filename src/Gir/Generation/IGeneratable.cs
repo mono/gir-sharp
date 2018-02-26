e5108ecf9d0dea45e20dec640f93fc5b3c25114e
Marius Ungureanu <marius.ungureanu@xamarin.com>
2018-02-26 09:57:26 +0200

    [Gen] Add C->C# name conversion

--- src/Gir/Generation/IGeneratable.cs
+++ src/Gir/Generation/IGeneratable.cs
@@ -14,4 +14,29 @@ namespace Gir
 	{
 		string CType { get; set; }
 	}
+
+	public static class ITypeExtensions
+	{
+		// Perf - maybe cache this?
+		public static string ToCSharp (this string cname)
+		{
+			// Capitalize the first letter, and parse for underscores, capitalizing the letters after them
+			var sb = new StringBuilder(cname.Length);
+
+			bool isUpper = true;
+			foreach (var c in cname)
+			{
+				if (c == '_')
+				{
+					isUpper = true;
+					continue;
+				}
+
+				sb.Append(isUpper ? char.ToUpper(c) : c);
+				isUpper = false;
+			}
+
+			return sb.ToString();
+		}
+	}
 }
