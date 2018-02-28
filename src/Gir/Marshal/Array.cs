77c744130c2631dc37673ee208b08926dbaad595
Stephen Shaw <sshaw@decriptor.com>
2018-02-28 01:47:50 -0700

    CType is in the Model partial class

--- src/Gir/Marshal/Array.cs
+++ src/Gir/Marshal/Array.cs
@@ -5,7 +5,5 @@ namespace Gir
 	public partial class Array : ISymbol
 	{
 		public string DefaultValue => throw new NotImplementedException();
-
-		string ISymbol.CType => throw new NotImplementedException();
 	}
 }
