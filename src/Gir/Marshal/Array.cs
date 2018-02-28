34f78b8ad5530bd273620646fb48653e08267a3d
Stephen Shaw <sshaw@decriptor.com>
2018-02-28 01:58:37 -0700

    Array and Constant shouldn't be ISymbol

--- src/Gir/Marshal/Array.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Gir
-{
-	public partial class Array : ISymbol
-	{
-		public string DefaultValue => throw new NotImplementedException();
-	}
-}
--- src/Gir/Marshal/Constant.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Gir
-{
-	public partial class Constant : ISymbol
-	{
-		public string DefaultValue => Value;
-	}
-}
