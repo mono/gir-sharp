220c0a51e38f0258e80586621180919356fe864a
Stephen Shaw <sshaw@decriptor.com>
2018-02-28 01:31:37 -0700

    Stub out Array

---
+++ src/Gir/Marshal/Array.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Gir
+{
+	public partial class Array : ISymbol
+	{
+		public string DefaultValue => throw new NotImplementedException();
+
+		string ISymbol.CType => throw new NotImplementedException();
+	}
+}
