6c1275a84a2ff0b66176ac19db9f80602ff317e6
Stephen Shaw <sshaw@decriptor.com>
2020-04-22 21:59:43 -0600

    Using new using resource syntax

--- src/Gir/Generation/Callback.cs
+++ src/Gir/Generation/Callback.cs
@@ -5,37 +5,35 @@ namespace Gir
 	{
 		public void Generate (GenerationOptions opts)
 		{
-			using (var writer = this.GetWriter (opts))
-			{
-				// TODO: Validation if we have an userdata parameter we can use so we can write this code.
-				// Otherwise, we need to generate code which uses a non-static callback.
+			using var writer = this.GetWriter (opts);
+			// TODO: Validation if we have an userdata parameter we can use so we can write this code.
+			// Otherwise, we need to generate code which uses a non-static callback.
 
-				var returnType = this.GetReturnCSharpType (writer);
-				var parameters = this.BuildParameters (opts, true);
+			var returnType = this.GetReturnCSharpType (writer);
+			var parameters = this.BuildParameters (opts, true);
 					
-				// Public API delegate which uses managed types.
-				writer.WriteLine ("[UnmanagedFunctionPointer (CallingConvention.Cdecl)]");
-				writer.WriteLine ($"public delegate {returnType} {Name} ({parameters.TypesAndNames})");
-				writer.WriteLine ();
+			// Public API delegate which uses managed types.
+			writer.WriteLine ("[UnmanagedFunctionPointer (CallingConvention.Cdecl)]");
+			writer.WriteLine ($"public delegate {returnType} {Name} ({parameters.TypesAndNames})");
+			writer.WriteLine ();
 
-				// Internal API delegate which uses unmanaged types.
-				writer.WriteLine ("[UnmanagedFunctionPointer (CallingConvention.Cdecl)]");
-				// TODO: Use native marshal types.
-				writer.WriteLine ($"internal delegate {returnType} {Name}Native ({parameters.TypesAndNames})");
-				writer.WriteLine ();
+			// Internal API delegate which uses unmanaged types.
+			writer.WriteLine ("[UnmanagedFunctionPointer (CallingConvention.Cdecl)]");
+			// TODO: Use native marshal types.
+			writer.WriteLine ($"internal delegate {returnType} {Name}Native ({parameters.TypesAndNames})");
+			writer.WriteLine ();
 
-				// Generate wrapper class - static if we can use gchandle, otherwise instance
-				// Check callback convention - async, notify, call
-				writer.WriteLine ($"internal static class {Name}Wrapper");
+			// Generate wrapper class - static if we can use gchandle, otherwise instance
+			// Check callback convention - async, notify, call
+			writer.WriteLine ($"internal static class {Name}Wrapper");
+			writer.WriteLine ("{");
+			using (writer.Indent ()) {
+				writer.WriteLine ($"public static void NativeCallback ({parameters.TypesAndNames})");
 				writer.WriteLine ("{");
-				using (writer.Indent ()) {
-					writer.WriteLine ($"public static void NativeCallback ({parameters.TypesAndNames})");
-					writer.WriteLine ("{");
-					// TODO: marshal params, call, handle exceptions
-					writer.WriteLine ("}");
-				}
+				// TODO: marshal params, call, handle exceptions
 				writer.WriteLine ("}");
 			}
+			writer.WriteLine ("}");
 		}
 	}
 }
--- src/Gir/Parser.cs
+++ src/Gir/Parser.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,9 +9,8 @@ namespace Gir
 
 		public static IEnumerable<Repository> Parse (string fileName, string includeDir, out Repository mainRepository)
 		{
-			using (var fs = File.OpenRead (fileName)) {
-				return Parse (fs, includeDir, out mainRepository);
-			}
+			using var fs = File.OpenRead (fileName);
+			return Parse (fs, includeDir, out mainRepository);
 		}
 
 		public static IEnumerable<Repository> Parse (Stream s, string includeDir, out Repository mainRepository)
@@ -32,14 +31,13 @@ namespace Gir
 				yield return repository;
 
 				foreach (var include in repository.Includes) {
-					using (var fs = File.OpenRead (Path.Combine (includeDir, include.GirName))) {
-						var serializer = new System.Xml.Serialization.XmlSerializer (typeof (Repository));
-						var repo = (Repository)serializer.Deserialize (fs);
+					using var fs = File.OpenRead (Path.Combine (includeDir, include.GirName));
+					var serializer = new System.Xml.Serialization.XmlSerializer (typeof (Repository));
+					var repo = (Repository)serializer.Deserialize (fs);
 
 
-						foreach (var incRepo in ParseRecursive (repo, includeDir, resolvedRepositories)) {
-							yield return incRepo;
-						}
+					foreach (var incRepo in ParseRecursive (repo, includeDir, resolvedRepositories)) {
+						yield return incRepo;
 					}
 				}
 			}
